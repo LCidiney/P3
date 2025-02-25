4c1fcf1db7fd80ceca14596d07c163c9d2cde370
LCidiney <lazarocidiney@gmail.com>
2025-02-24 21:01:15 -0300

    remake part1

--- pages/login/assets/js/script.js
+++ pages/login/assets/js/script.js
@@ -1,166 +1,82 @@
-document.querySelector('.btn-transf').addEventListener('click', attSection);
-document.querySelectorAll('input[name="opcoes"]').forEach(radio => {
-    radio.addEventListener('click', handleButtonClick);
-});
+const form = document.querySelector(".login");
+let users;
+let user;
 
-function attSection() {
-    let content = document.querySelector('.content');
-    let login = document.querySelector('.login');
-    let telaOpcoes = document.querySelector('.tela-opcoes');
-    let telaResultado = document.querySelector('#tela-resultado');
-    let button = document.querySelector('.btn-transf');
-    if (content.style.display === "flex") {
-        content.style.display = "none";
-        login.style.display = "flex";
-        telaOpcoes.style.display = "none";
-        telaResultado.style.display = "none";
-        button.innerHTML = "<b>LOGAR</b>";
-    } else if (login.style.display === "flex") {
-        let nome = document.querySelector('#nome').value.trim();
-        let cpf = document.querySelector('#cpf').value.trim();
-        if (!nome || !cpf) {
-            alert("Por favor, preencha todos os campos antes de continuar.");
-            return;
-        };
-        localStorage.setItem('nome', nome);
-        localStorage.setItem('cpf', cpf);
-        if (!verificarAcesso()) {
-            return;
-        };
-        login.style.display = "none";
-        telaOpcoes.style.display = "flex";
-        telaResultado.style.display = "none";
-        button.innerHTML = "<b>INVESTIR</b>";
-    } else if (telaOpcoes.style.display === "flex") {
-        let selectOption = document.querySelector('input[name="opcoes"]:checked');
-        if (!selectOption) {
-            alert("Por favor, selecione uma opção antes de continuar.");
-            return;
-        };
-        localStorage.setItem('invest', selectOption.value);
-        calcInvest();
-        geraParagrafos();
-        telaOpcoes.style.display = "none";
-        telaResultado.style.display = "block";
-        button.innerHTML = "<b>INICIAR APLICAÇÃO</b>";
-    } else {
-        content.style.display = "flex";
-        login.style.display = "none";
-        telaOpcoes.style.display = "none";
-        telaResultado.style.display = "none";
-        button.innerHTML = "<b>INICIAR APLICAÇÃO</b>";
-        JSON.parse(localStorage.getItem('cpf'));
-        localStorage.removeItem('cpf');
-    };
-};
-
-function handleButtonClick() {
-    let telaOpcoes = document.querySelector('.tela-opcoes');
-    if (telaOpcoes.style.display === "flex") {
-        let selectOption = document.querySelector('input[name="opcoes"]:checked');
-        if (!selectOption) {
-            alert("Por favor, selecione uma opção antes de continuar.");
-            return;
-        }
+class User {
+    constructor(name, cpf) {
+        this.name = name;
+        this.cpf = cpf;
     }
-
-};
-
-function geraParagrafos() {
-    let telaResult = document.querySelector('#resultado-lucro');
-    let invest = localStorage.getItem('invest');
-    let nome = localStorage.getItem('nome');
-    let cpf = localStorage.getItem('cpf');
-    let retornoUsuario = localStorage.getItem('retornoUsuario');
-    telaResult.innerHTML = "";
-
-    if (retornoUsuario > 0) {
-        let msgs = [
-            `PARABÉNS, VOCÊ GANHOU!!!`,
-            `Nome: ${nome}`,
-            `CPF: ${cpf}`,
-            `Valor investido: R$${invest},00`,
-            `Valor retornado: R$${retornoUsuario}`
-        ];
-        msgs.forEach(msg => {
-            let paragrafo = document.createElement('p');
-            paragrafo.textContent = msg;
-            telaResult.appendChild(paragrafo);
-        });
-    } else {
-        let msgs = [
-            `PERDEU OTÁRIO!!!`,
-            `Nome: ${nome}`,
-            `CPF: ${cpf}`,
-            `Valor investido: R$${invest},00`,
-            `Valor roubado: R$${invest},00`,
-        ];
-        msgs.forEach(msg => {
-            let paragrafo = document.createElement('p');
-            paragrafo.textContent = msg;
-            telaResult.appendChild(paragrafo);
-        });
-        let imagem = document.createElement('img');
-        imagem.src = "./assets/imgs/urubu_voando.jpg";
-        imagem.alt = "urubu_voando";
-        imagem.classList.add('urubu_voando');
-        telaResult.appendChild(imagem);
-    };
 }
 
-function calcInvest() {
-    let invest = parseInt(localStorage.getItem('invest'));
-    let retornoUsuario = 0;
-    let lucroUrubu = parseInt(localStorage.getItem('lucroUrubu')) || 0;
-    let caixaUrubu = parseInt(localStorage.getItem('caixaUrubu')) || 10000;
-    if (invest === 100 && lucroUrubu > invest) {
-        retornoUsuario = invest + 150;
-        lucroUrubu = lucroUrubu - 150;
-        caixaUrubu = caixaUrubu - 150;
-    } else if (invest === 150 && lucroUrubu > invest) {
-        retornoUsuario = invest + 150;
-        lucroUrubu = lucroUrubu - 150;
-        caixaUrubu = caixaUrubu - 150;
-    } else if (invest === 200 && lucroUrubu > invest) {
-        retornoUsuario = invest + 350;
-        lucroUrubu = lucroUrubu - 350;
-        caixaUrubu = caixaUrubu - 350;
-    } else if (invest === 350 && lucroUrubu > invest) {
-        retornoUsuario = invest + 350;
-        lucroUrubu = lucroUrubu - 350;
-        caixaUrubu = caixaUrubu - 350;
-    } else if (invest === 400 && lucroUrubu > invest) {
-        retornoUsuario = invest + 550;
-        lucroUrubu = lucroUrubu - 550;
-        caixaUrubu = caixaUrubu - 550;
-    } else if (invest === 550 && lucroUrubu > invest) {
-        retornoUsuario = invest + 550;
-        lucroUrubu = lucroUrubu - 550;
-        caixaUrubu = caixaUrubu - 550;
-    } else {
-        caixaUrubu = caixaUrubu + invest;
-        lucroUrubu = lucroUrubu + invest;
-        retornoUsuario = retornoUsuario - invest;
-        let cpfsBloqueados = JSON.parse(localStorage.getItem('cpfsBloqueados')) || [];
-        let cpf = JSON.parse(localStorage.getItem('cpf'));
-        if (!cpfsBloqueados.includes(cpf)) {
-            cpfsBloqueados.push(cpf);
-            localStorage.setItem('cpfsBloqueados', JSON.stringify(cpfsBloqueados));
-        }
-        }
-    localStorage.setItem('retornoUsuario', retornoUsuario);
-    localStorage.setItem('lucroUrubu', lucroUrubu);
-    localStorage.setItem('caixaUrubu', caixaUrubu);
-};
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    isValidCPF(cpf);
+    createUser();
 
-function verificarAcesso() {
-    let cpf = JSON.parse(localStorage.getItem('cpf'));
-    let cpfsBloqueados = JSON.parse(localStorage.getItem('cpfsBloqueados')) || [];
+});
 
-    if (cpfsBloqueados.includes(cpf)) {
-        alert("Acesso bloqueado para este CPF.");
+function createUser() {
+    try {
+        const name = document.querySelector("#name").value.trim();
+        const cpf = document.querySelector("#cpf").value;
+        
+        if (!name || !cpf) {
+            throw new Error("Por favor, preencha todos os campos");
+        }
+        
+        if (!isValidCPF(cpf)) {
+            throw new Error("CPF inválido");
+        }
+        
+        user = JSON.stringify(new User(name, cpf));
+        if (!accessValidator(user)) {
+            usersControl(user);
+        } else {
+            alert("Usuário com este CPF já existe!");
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+function usersControl(user) {
+    users = JSON.parse(localStorage.getItem('users')) || [];
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Usuário cadastrado com sucesso!");
+    window.location.href = "../opcoes/index.html";
+}
+
+function accessValidator(user) {
+    const parsedUsers = JSON.parse(user);
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    return users.some(storedUser => {
+        const parsedStoredUser = JSON.parse(storedUser);
+        return parsedStoredUser.cpf === parsedUsers.cpf;
+    });
+}
+
+function isValidCPF(cpf) {
+    if (!cpf || typeof cpf !== 'string') {
         return false;
     }
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11) return false;
+    if (/^(\d)\1+$/.test(cpf)) return false;
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.charAt(9))) return false;
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.charAt(10))) return false;
     return true;
-};
-
+}
